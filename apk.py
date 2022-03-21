831d308bd7049c581e7082b66a5835cb93e7c77e
AniketDasSushil <85302839+AniketDasSushil@users.noreply.github.com>
2022-03-21 21:43:59 +0530

    Update apk.py

--- apk.py
+++ apk.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import streamlit as st
+import plotly.express as px
 st.title('Data Analyzer')
 st.caption('~made by aniket das')
 file = st.file_uploader('upload your csv file here')
 if file:
     x = pd.read_csv(file)
     base_column = st.selectbox(label='select the base column',options=x.columns)
-    analysis_column = st.selectbox(label='select the analysis column',options=x.columns)
+    analysis_column = st.selectbox(label='select the analysis column',options=x.columns,)
     analysis = x.groupby(base_column)[analysis_column].value_counts(normalize=True).to_frame()
     analysis = analysis.unstack().reset_index()
     analysis = analysis.set_index(base_column)
@@ -14,5 +15,11 @@ if file:
     col = analysis.columns
     analysis[col] = analysis[col].applymap('{:,.2f}%'.format)
     st.dataframe(analysis)
+    st.header(analysis_column)
+    fig = px.bar(x=x[analysis_column],color=x[base_column],barmode='group',template='presentation')
+    fig.update_layout(autosize=False,width=800,height=400)
+    st.plotly_chart(fig)
+    
     data = analysis.to_csv().encode('utf-8')
-    st.download_button('download',data,'file.csv','text/csv',key='download-csv')
+    
+st.download_button('download',data,'file.csv','text/csv',key='download-csv')
