45e7d690650cd69f49bcfcae0d8656f39269ec5f
AniketDasSushil <85302839+AniketDasSushil@users.noreply.github.com>
2022-03-22 08:31:34 +0530

    Update apk.py

--- apk.py
+++ apk.py
@@ -5,21 +5,30 @@ st.title('Data Analyzer')
 st.caption('~made by aniket das')
 file = st.file_uploader('upload your csv file here')
 if file:
-    x = pd.read_csv(file)
+    try:
+        x = pd.read_csv(file)
+    except ValueError:
+        st.error('error in file format')
     base_column = st.selectbox(label='select the base column',options=x.columns)
     analysis_column = st.selectbox(label='select the analysis column',options=x.columns,)
     analysis = x.groupby(base_column)[analysis_column].value_counts(normalize=True).to_frame()
+    dis = analysis.copy()
     analysis = analysis.unstack().reset_index()
     analysis = analysis.set_index(base_column)
     analysis = analysis.T*100
     col = analysis.columns
     analysis[col] = analysis[col].applymap('{:,.2f}%'.format)
     st.dataframe(analysis)
-    st.header(analysis_column)
-    fig = px.bar(x=x[analysis_column],color=x[base_column],barmode='group',template='presentation')
-    fig.update_layout(autosize=False,width=800,height=400)
-    st.plotly_chart(fig)
-    
     data = analysis.to_csv().encode('utf-8')
-    
     st.download_button('download',data,'file.csv','text/csv',key='download-csv')
+    if base_column != analysis_column:
+        dis.rename(columns={analysis_column:'percent'},inplace=True)
+        dis = dis.reset_index()
+        dis['percent'] = dis['percent']*100
+        fig = px.bar(y=dis['percent'],x=dis[base_column],title=analysis_column,color=dis[analysis_column],template='presentation',labels={'y':'percent','x':base_column},text=dis['percent'].apply(lambda x: "{0:1.2f}%".format(x)))
+       
+        st.plotly_chart(fig)
+    
+    
+    
+    
